13d5b5eec74663b91929a0ef68bc78550e4e62ae
jianhui.yang <jianhui.yang@medsci.cn>
2020-07-08 18:12:16 +0800

    fix

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.example.common.dto.ResultT;
 import com.example.order.service.OrderFormService;
 import com.example.order.vo.OrderFormVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/order")
 @RestController
+@RefreshScope
 public class OrderController {
     @Autowired
     private OrderFormService orderFormService;
@@ -19,5 +22,13 @@ public class OrderController {
     public ResultT<OrderFormVO> getById(@PathVariable String id) {
         OrderFormVO orderFormVO = orderFormService.getById(id);
         return ResultT.success(orderFormVO);
+    }
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
     }
 }
--- order/src/test/java/com/example/order/OrderApplicationTests.java
+++ order/src/test/java/com/example/order/OrderApplicationTests.java
@@ -1,16 +1,12 @@
 package com.example.order;
 
 import com.alibaba.fastjson.JSON;
-import com.example.order.pojo.OrderForm;
 import com.example.order.request.PlaceOrderRequest;
 import com.example.order.service.OrderFormService;
 import com.example.order.vo.OrderFormVO;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 @SpringBootTest
 class OrderApplicationTests {
